73a8ff391af6d6281db1ec1d417d9e227b51eee6
script0rium <jad_phil@yahoo.com>
2022-02-07 17:25:57 +0000

    first blog post!

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220205.2314';
+const cacheName = 'chirpy-20220208.0124';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
