e670681a52583f7e994950f0f479b1ae8fbd41d7
script0rium <jad_phil@yahoo.com>
2022-02-16 17:37:22 +0000

    bash update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220217.0109';
+const cacheName = 'chirpy-20220217.0136';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
