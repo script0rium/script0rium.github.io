c7fedc6f753a2f07893f7766003018f1dc358fa1
script0rium <jad_phil@yahoo.com>
2022-06-24 20:31:27 +0100

    text tweaks

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220625.0148';
+const cacheName = 'chirpy-20220625.0331';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
