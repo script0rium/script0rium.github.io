d3c2880dd0e39a9dccd33a332edf1481c51441be
script0rium <jad_phil@yahoo.com>
2022-07-06 21:27:31 +0100

    10 bash comds

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220707.0026';
+const cacheName = 'chirpy-20220707.0426';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
